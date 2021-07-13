59282c31828c9bc4a12a122a047680a3e2f97ecf
Ramneet96 <kaurramneet1444@gmail.com>
2021-07-13 12:12:48 -0700

    Initial commit

---
+++ src/com/company/ComputeWage.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ComputeWage {
+
+    private String name;
+    private double gtotal;
+    private int hours;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGtotal() {
+        return gtotal;
+    }
+
+    public void setGtotal(double gtotal) {
+        this.gtotal = gtotal;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void acceptData() {
+        boolean inputValid = false;
+
+        Scanner key = new Scanner(System.in);
+
+        System.out.println("enter the name");
+        name = key.nextLine();
+
+        while (!inputValid) {
+            System.out.println("enter hours ");
+            String input = key.next();
+
+            try {
+
+                hours = Integer.parseInt(input);
+                inputValid = true;
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("You didn't enter a valid number");
+
+            }
+        }
+    }
+
+            public double computeWage()
+            {
+
+                if(this.hours>40){
+                    gtotal=(40*15)+(16.5*(hours-40));
+
+
+                }else{
+                    gtotal=hours*15;
+                }
+                return gtotal;
+            }
+
+            public void display(){
+                System.out.println("the total wage of "+this.name+" is "+this.gtotal);
+            }
+        }
+
+
+
+
